11408a383fd58ee61882e421f9e5e69a43eb48c6
steveBstorm <steve.lorent@bstorm.be>
2022-01-28 16:38:05 +0100

    Ajoutez des fichiers projet.

---
+++ TestAdoToolbox/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdoToolbox;
+using TestAdoToolbox;
+
+Console.WriteLine("Hello, World!");
+
+string cs = @"Data Source=DESKTOP-RGPQP6I\TFTIC2019;Initial Catalog=ADOStudent;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+//Connection cnx = new Connection(cs);
+//Command cmd = new Command("INSERT INTO maTable (Nom) VALUES (@nom)");
+//cmd.AddParameter("nom", "Arthur");
+
+//Console.WriteLine(cnx.ExecuteNonQuery(cmd)); 
+
+Connection cnx = new Connection(cs);
+string sql = "SELECT * FROM Section";
+Command cmd = new Command(sql);
+
+foreach (Section item in cnx.ExecuteReader(cmd, Section.Converter))
+{
+    Console.WriteLine(item.Id + " - " + item.SectionName);
+}
---
+++ TestAdoToolbox/Section.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAdoToolbox
+{
+    public class Section
+    {
+        public int Id { get; set; }
+        public string SectionName { get; set; }
+
+        public static Section Converter(SqlDataReader reader)
+        {
+            return new Section
+            {
+                Id = (int)reader["Id"],
+                SectionName = (string)reader["SectionName"]
+            };
+        }
+    }
+}
